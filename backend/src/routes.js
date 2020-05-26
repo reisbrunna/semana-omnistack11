405425396780938ce23e076c69ebb73608297429
Brunna Reis <reiisbrunna@gmail.com>
2020-05-26 14:55:04 -0300

    -

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const CasosController = require('./controllers/CasosController');
 const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const SessionController = require('./controllers/Session Controller');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY ]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}),  OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
