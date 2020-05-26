9f3c030cc97a6044568481e1194989b3c86d028c
Brunna Reis <reiisbrunna@gmail.com>
2020-05-26 19:52:59 -0300

    -

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // importando a rota
 
 
@@ -7,6 +8,7 @@ const app = express(); //armazena a aplicação
 app.use(cors());
 app.use(express.json())
 app.use(routes);
+app.use(errors());
 app.listen(3333);// porta de acesso  
 
 /*
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const CasosController = require('./controllers/CasosController');
 const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/Session Controller');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -13,19 +13,36 @@ routes.post('/sessions', SessionController.create);
 routes.get('/ongs', OngController.index);
 
 routes.post('/ongs', celebrate({
-    [Segments.BODY ]: Joi.object().keys({
+    [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
         whatsapp: Joi.number().required().min(10).max(11),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
-}),  OngController.create);
+}), OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
 
 routes.post('/casos', CasosController.create);
-routes.get('/casos', CasosController.index);
-routes.delete('/casos/:id', CasosController.delete);
+
+routes.get('/casos', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), CasosController.index);
+
+
+
+routes.delete('/casos/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), CasosController.delete);
+
 
 module.exports = routes; // exporta uma váriavel dentro de um arquivo
